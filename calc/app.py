74d164f1b488044760ff4d54015f559dfbf1c332
stevan <stevangrubac123@gmail.com>
2022-02-10 18:55:24 -0500

    Commit calculator app

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,34 @@
 # Put your app in here.
+from flask import Flask, request
+from operations import *
+
+app = Flask(__name__)
+
+
+@app.route('/add')
+def addRoute():
+    a = request.args["a"]
+    b = request.args["b"]
+    c = add(int(a),int(b))
+    return str(c)
+
+@app.route('/sub')
+def subRoute():
+    a = request.args["a"]
+    b = request.args["b"]
+    c = sub(int(a),int(b))
+    return str(c)
+
+@app.route('/mult')
+def multRoute():
+    a = request.args["a"]
+    b = request.args["b"]
+    c = mult(int(a),int(b))
+    return str(c)
+
+@app.route('/div')
+def divRoute():
+    a = request.args["a"]
+    b = request.args["b"]
+    c = div(int(a),int(b))
+    return str(c)
